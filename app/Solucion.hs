13e70d30532fbc125b92921817b42540042fe7b3
smora <santiagomorad@gmail.com>
2023-05-16 19:36:34 -0300

    propuestas para ejercicio 8 y 9, se dejan en archivo app/Solucion.hs

--- app/Solucion.hs
+++ app/Solucion.hs
@@ -218,24 +218,24 @@ lesGustanLasMismasPublicaciones red u1 u2 = publicacionesQueLeGustanA red u1 == 
 -- Si un usuario tiene un seguidor fiel, a este, en particular le gusta la primer publicacion. Por eso, se toman los usuarios a los que
 -- les gusta la primer publicacion y despues se verifica si les gustan las otras publicaciones del usuario indicado.
 
-tieneUnSeguidorFiel :: RedSocial -> Usuario -> Bool
-tieneUnSeguidorFiel red u = hayAlgunSeguidorFiel (publicacionesDe red u) (seguidoresPrimerPublicacion red u)
-
-
-seguidoresPrimerPublicacion :: RedSocial -> Usuario -> [Usuario]
-seguidoresPrimerPublicacion red u | publicacionesDe red u == [] = []
-                                  | otherwise = likesDePublicacion (head (publicacionesDe red u))
-                                  
-hayAlgunSeguidorFiel :: [Publicacion] -> [Usuario] -> Bool
-hayAlgunSeguidorFiel _ [] = False
-hayAlgunSeguidorFiel pubs (us:uss) | us `leGustanTodas` pubs = True
-                                   | otherwise = hayAlgunSeguidorFiel pubs uss
-
-leGustanTodas :: Usuario -> [Publicacion] -> Bool 
-leGustanTodas usuario [] = True 
-leGustanTodas usuario (pub:pubs) | aUsuarioLeGustaPub = leGustanTodas usuario pubs 
-                                 | otherwise = False
-                                 where aUsuarioLeGustaPub = usuario `perteneceA` (likesDePublicacion pub)
+-- tieneUnSeguidorFiel :: RedSocial -> Usuario -> Bool
+-- tieneUnSeguidorFiel red u = hayAlgunSeguidorFiel (publicacionesDe red u) (seguidoresPrimerPublicacion red u)
+--
+--
+-- seguidoresPrimerPublicacion :: RedSocial -> Usuario -> [Usuario]
+-- seguidoresPrimerPublicacion red u | publicacionesDe red u == [] = []
+--                                   | otherwise = likesDePublicacion (head (publicacionesDe red u))
+--
+-- hayAlgunSeguidorFiel :: [Publicacion] -> [Usuario] -> Bool
+-- hayAlgunSeguidorFiel _ [] = False
+-- hayAlgunSeguidorFiel pubs (us:uss) | us `leGustanTodas` pubs = True
+--                                    | otherwise = hayAlgunSeguidorFiel pubs uss
+--
+-- leGustanTodas :: Usuario -> [Publicacion] -> Bool
+-- leGustanTodas usuario [] = True
+-- leGustanTodas usuario (pub:pubs) | aUsuarioLeGustaPub = leGustanTodas usuario pubs
+--                                  | otherwise = False
+--                                  where aUsuarioLeGustaPub = usuario `perteneceA` (likesDePublicacion pub)
 
 
 ------------------
@@ -246,26 +246,91 @@ leGustanTodas usuario (pub:pubs) | aUsuarioLeGustaPub = leGustanTodas usuario pu
 -- existiria una secuencia si tienen un amigo en comun (u), en ese caso la lista seria del tipo [u1, u, us2]
 
 
+-- existeSecuenciaDeAmigos :: RedSocial -> Usuario -> Usuario -> Bool
+-- existeSecuenciaDeAmigos red u1 u2 | seRelacionanEntreSi = True
+--                                   | tienenAmigosEnComun = True
+--                                   | otherwise = False
+--                                   where seRelacionanEntreSi = relacionados (relaciones red) u1 u2
+--                                         tienenAmigosEnComun = amigosEnComun (amigosDe red u1) (amigosDe red u2)
+--
+--
+-- relacionados :: [Relacion] -> Usuario -> Usuario -> Bool
+-- relacionados [] u1 u2 = False
+-- relacionados (rel:rels) u1 u2 | rel == (u1, u2) || rel == (u1, u2) = True
+--                               | otherwise = relacionados rels u1 u2
+--
+--
+-- dentroDeUnaLista :: [Usuario] -> Usuario -> Bool
+-- dentroDeUnaLista [] _ = False
+-- dentroDeUnaLista (us:uss) u | us == u = True
+--                             | otherwise = dentroDeUnaLista uss u
+--
+-- amigosEnComun :: [Usuario] -> [Usuario] -> Bool
+-- amigosEnComun l [] = False
+-- amigosEnComun l (u:us) | dentroDeUnaLista l u = True
+--                        | otherwise = amigosEnComun l us
+
+-------------------------------
+-- PROPUESTA EJERCICIOS 9 Y 10
+-------------------------------
+pertenece :: (Eq t) => [t] -> t -> Bool
+pertenece [] y = False
+pertenece (x:xs) y | x == y = True
+                   | otherwise = pertenece xs y
+longitud :: [t] -> Int
+longitud [] = 0
+longitud (x:xs) = 1 + longitud xs
+-- Ejercicio 8: decide si a dos usuarios les gustan las mismas publicaciones
+interseccion :: (Eq t) => [t] -> [t] -> [t]
+interseccion [] ys = []
+interseccion (x:xs) ys | pertenece ys x = [x] ++ interseccion xs ys
+                       | otherwise = interseccion xs ys
+
+-- Ejercicio 9: decide si un usuario tiene un seguidor al que le gustan todas sus publicaciones
+likesEnComun :: [Publicacion] -> [Usuario]
+likesEnComun (p:[]) = likesDePublicacion p
+likesEnComun (p:ps) = interseccion (likesDePublicacion p) (likesEnComun ps)
+tieneUnSeguidorFiel :: RedSocial -> Usuario -> Bool
+tieneUnSeguidorFiel rs u = longitud (likesEnComun (publicacionesDe rs u)) > 0
+
+-- Ejercicio 10: Indica si existe una cadena de relaciones con la cual desde el usuario 1 se llega al usuario 2
+-- permutaciones
+insertar :: t -> Int -> [t] -> [t]
+insertar w 0 xs = [w] ++ xs
+insertar w at (x:xs) = [x] ++ insertar w (at-1) xs
+perm :: t -> [t] -> Int-> [[t]]
+perm y xs 0 = [[y] ++ xs]
+perm y xs l = [insertar y l xs] ++ perm y xs (l-1)
+permarray :: t -> [[t]] -> [[t]]
+permarray x [] = []
+permarray x (a:ac) = perm x a (longitud a) ++ permarray x ac
+_permutaciones :: [t] -> [[t]] -> [[t]]
+_permutaciones [] ac = ac
+_permutaciones (x:xs) ac = _permutaciones xs (permarray x ac)
+permutaciones :: [t] -> [[t]]
+permutaciones xs = _permutaciones xs [[]]
+-- posicion de un elemento en una lista
+posicion :: (Eq t) => [t] -> t -> Int
+posicion (x:xs) y | x == y = 0
+                  | otherwise = 1 + posicion xs y
+-- trozo de una lista desde indice fr hasta indice to
+slice :: [t] -> Int -> Int -> [t]
+slice [] fr to = []
+slice (x:xs) 0 0 = []
+slice (x:xs) 0 to = [x] ++ slice xs 0 (to-1)
+slice xs fr to = slice xs (fr-1) (to-1)
+-- verifica que las relaciones todas las relaciones de la cadena de usuarios existan entre las Relaciones de la red social
+relacionesEnCadena :: [Relacion] -> [Usuario] -> Bool
+relacionesEnCadena rs (u:us) | longitud us>1 = (pertenece rs r1 || pertenece rs r2) && relacionesEnCadena rs us
+                             | otherwise = (pertenece rs r1 || pertenece rs r2)
+                             where r1 = (u, head us)
+                                   r2 = (head us, u)
+-- toma cadenas de amigos validas a partir de las permutaciones de usuarios
+cadenaDeAmigos :: [Relacion] -> [[Usuario]] -> Usuario -> Usuario -> [Usuario]
+cadenaDeAmigos rs [] u1 u2 = []
+cadenaDeAmigos rs (a:as) u1 u2 | longitud s > 0 && relacionesEnCadena rs s = s
+                               | otherwise = cadenaDeAmigos rs as u1 u2
+                               where s = slice a (posicion a u1) (posicion a u2)
+-- toma cadenas de amigos validas a partir de las permutaciones de usuarios
 existeSecuenciaDeAmigos :: RedSocial -> Usuario -> Usuario -> Bool
-existeSecuenciaDeAmigos red u1 u2 | seRelacionanEntreSi = True
-                                  | tienenAmigosEnComun = True
-                                  | otherwise = False
-                                  where seRelacionanEntreSi = relacionados (relaciones red) u1 u2
-                                        tienenAmigosEnComun = amigosEnComun (amigosDe red u1) (amigosDe red u2)
-
-
-relacionados :: [Relacion] -> Usuario -> Usuario -> Bool
-relacionados [] u1 u2 = False
-relacionados (rel:rels) u1 u2 | rel == (u1, u2) || rel == (u1, u2) = True
-                              | otherwise = relacionados rels u1 u2
-                              
-
-dentroDeUnaLista :: [Usuario] -> Usuario -> Bool
-dentroDeUnaLista [] _ = False
-dentroDeUnaLista (us:uss) u | us == u = True
-                            | otherwise = dentroDeUnaLista uss u
-
-amigosEnComun :: [Usuario] -> [Usuario] -> Bool                         
-amigosEnComun l [] = False
-amigosEnComun l (u:us) | dentroDeUnaLista l u = True 
-                       | otherwise = amigosEnComun l us
+existeSecuenciaDeAmigos rs u1 u2 = longitud (cadenaDeAmigos (relaciones rs) (permutaciones (usuarios rs)) u1 u2) > 0
