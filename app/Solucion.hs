7b16c963b2195dec29eabb79454129911a87f8ce
smora <santiagomorad@gmail.com>
2023-05-18 01:31:38 -0300

    version preliminar solucion.hs

--- app/Solucion.hs
+++ app/Solucion.hs
@@ -58,27 +58,17 @@ sumaElementosDeLista (u:us) = 1 + sumaElementosDeLista us
 -- Ejercicio 4 --
 -----------------
 
--- Se accede a los usuarios de la red con la funcion basica usuarios. Se usa la funcion cantidadDeAmigos y se forman tuplas con el usuario 
--- y la cantidad de amigos. Se agregan en una lista y luego se determina cual es la tupla con mas amigos. Luego se proyecta el usuario es esta.
+-- compara los usuarios de la red social apoyandose en la funcion conMasAmigos, que decide cual de los dos usuarios de entrada tiene mas amigos
 
+conMasAmigos :: RedSocial -> Usuario -> Usuario -> Usuario
+conMasAmigos rs u1 u2 | (cantidadDeAmigos rs u1) >= (cantidadDeAmigos rs u2) = u1
+                      | otherwise = u2
+--    implementacion usuario con mas amigos en la red social
+_usuarioConMasAmigos :: [Usuario] -> RedSocial -> Usuario
+_usuarioConMasAmigos (u:[]) rs = u
+_usuarioConMasAmigos (u:us) rs = conMasAmigos rs u (_usuarioConMasAmigos us rs)
 usuarioConMasAmigos :: RedSocial -> Usuario
-usuarioConMasAmigos red = fst (mayorCantidadAmigos (conteoDeAmigos (usuarios red) red))
-
-
-conteoDeAmigos :: [Usuario] -> RedSocial -> [(Usuario, Int)]
-conteoDeAmigos [] red = []
-conteoDeAmigos (x:xs) red = (x, cantidadDeAmigos red x) : conteoDeAmigos xs red
-
-
-mayorCantidadAmigos :: [(Usuario, Int)] -> (Usuario, Int) -- Devuelve la tupla con la mayor segunda componente
-mayorCantidadAmigos (x:[]) = x
-mayorCantidadAmigos (x:xs) = mayorCantidadAmigos (mayor x (head xs) : tail xs)
-
-
-mayor :: (Usuario, Int) -> (Usuario, Int) -> (Usuario, Int) -- Compara dos tuplas y devuelve la que tiene segunda componente mas grande
-mayor (us1, a) (us2, b) | a >= b = (us1, a)
-                        | a < b = (us2, b)
-
+usuarioConMasAmigos rs = _usuarioConMasAmigos (usuarios rs) rs
 
 -----------------
 -- Ejercicio 5 --
@@ -88,8 +78,7 @@ mayor (us1, a) (us2, b) | a >= b = (us1, a)
 -- Utilizamos la funcion mayorCantidadAmigos y proyectamos la cantidad. Luego se determina si esta es mayor que diez.
 
 estaRobertoCarlos :: RedSocial -> Bool
-estaRobertoCarlos ([],_,_) = False
-estaRobertoCarlos red = snd (mayorCantidadAmigos (conteoDeAmigos (usuarios red) red)) > 10
+estaRobertoCarlos red = (cantidadDeAmigos red (usuarioConMasAmigos red)) > 10
 
 
 -----------------
@@ -133,8 +122,7 @@ publicacionesDeInteres (pub:pubs) u | aUsuarioLeGustaPub = pub : publicacionesDe
 perteneceA :: Usuario -> [Usuario] -> Bool
 perteneceA u [] = False
 perteneceA u (x:xs) | x == u = True
-                     | x /= u = perteneceA u xs
-
+                    | x /= u = perteneceA u xs
 
 -----------------
 -- Ejercicio 8 --
@@ -153,119 +141,46 @@ lesGustanLasMismasPublicaciones red u1 u2 = publicacionesQueLeGustanA red u1 == 
 -- Si un usuario tiene un seguidor fiel, a este, en particular le gusta la primer publicacion. Por eso, se toman los usuarios a los que
 -- les gusta la primer publicacion y despues se verifica si les gustan las otras publicaciones del usuario indicado.
 
--- tieneUnSeguidorFiel :: RedSocial -> Usuario -> Bool
--- tieneUnSeguidorFiel red u = hayAlgunSeguidorFiel (publicacionesDe red u) (seguidoresPrimerPublicacion red u)
---
---
--- seguidoresPrimerPublicacion :: RedSocial -> Usuario -> [Usuario]
--- seguidoresPrimerPublicacion red u | publicacionesDe red u == [] = []
---                                   | otherwise = likesDePublicacion (head (publicacionesDe red u))
---
--- hayAlgunSeguidorFiel :: [Publicacion] -> [Usuario] -> Bool
--- hayAlgunSeguidorFiel _ [] = False
--- hayAlgunSeguidorFiel pubs (us:uss) | us `leGustanTodas` pubs = True
---                                    | otherwise = hayAlgunSeguidorFiel pubs uss
---
--- leGustanTodas :: Usuario -> [Publicacion] -> Bool
--- leGustanTodas usuario [] = True
--- leGustanTodas usuario (pub:pubs) | aUsuarioLeGustaPub = leGustanTodas usuario pubs
---                                  | otherwise = False
---                                  where aUsuarioLeGustaPub = usuario `perteneceA` (likesDePublicacion pub)
+tieneUnSeguidorFiel :: RedSocial -> Usuario -> Bool
+tieneUnSeguidorFiel red u = hayAlgunSeguidorFiel (publicacionesDe red u) (seguidoresPrimerPublicacion red u)
 
+
+seguidoresPrimerPublicacion :: RedSocial -> Usuario -> [Usuario]
+seguidoresPrimerPublicacion red u | publicacionesDe red u == [] = []
+                                  | otherwise = likesDePublicacion (head (publicacionesDe red u))
+
+hayAlgunSeguidorFiel :: [Publicacion] -> [Usuario] -> Bool
+hayAlgunSeguidorFiel _ [] = False
+hayAlgunSeguidorFiel pubs (us:uss) | us `leGustanTodas` pubs = True
+                                   | otherwise = hayAlgunSeguidorFiel pubs uss
+
+leGustanTodas :: Usuario -> [Publicacion] -> Bool
+leGustanTodas usuario [] = True
+leGustanTodas usuario (pub:pubs) | aUsuarioLeGustaPub = leGustanTodas usuario pubs
+                                 | otherwise = False
+                                 where aUsuarioLeGustaPub = usuario `perteneceA` (likesDePublicacion pub)
 
 ------------------
 -- Ejercicio 10 --
 ------------------
+-- Dado el primer elementos de una posible secuencia, busco los candidatos a segundos elementos. Estos van a ser los amigos del primero.
+-- Asi voy avanzando en las siguientes posiciones quitando los usuarios ya evaluados y viendo si esta el usuario final.
 
--- Existe una secuencia de amigos entre dos usuarios si estos se relacionan entre si, la lista en ese caso podria ser [u1, u2]. Tambien 
--- existiria una secuencia si tienen un amigo en comun (u), en ese caso la lista seria del tipo [u1, u, us2]
-
-
--- existeSecuenciaDeAmigos :: RedSocial -> Usuario -> Usuario -> Bool
--- existeSecuenciaDeAmigos red u1 u2 | seRelacionanEntreSi = True
---                                   | tienenAmigosEnComun = True
---                                   | otherwise = False
---                                   where seRelacionanEntreSi = relacionados (relaciones red) u1 u2
---                                         tienenAmigosEnComun = amigosEnComun (amigosDe red u1) (amigosDe red u2)
---
---
--- relacionados :: [Relacion] -> Usuario -> Usuario -> Bool
--- relacionados [] u1 u2 = False
--- relacionados (rel:rels) u1 u2 | rel == (u1, u2) || rel == (u1, u2) = True
---                               | otherwise = relacionados rels u1 u2
---
---
--- dentroDeUnaLista :: [Usuario] -> Usuario -> Bool
--- dentroDeUnaLista [] _ = False
--- dentroDeUnaLista (us:uss) u | us == u = True
---                             | otherwise = dentroDeUnaLista uss u
---
--- amigosEnComun :: [Usuario] -> [Usuario] -> Bool
--- amigosEnComun l [] = False
--- amigosEnComun l (u:us) | dentroDeUnaLista l u = True
---                        | otherwise = amigosEnComun l us
-
--------------------------------
--- PROPUESTA EJERCICIOS 9 Y 10
--------------------------------
-pertenece :: (Eq t) => [t] -> t -> Bool
-pertenece [] y = False
-pertenece (x:xs) y | x == y = True
-                   | otherwise = pertenece xs y
-longitud :: [t] -> Int
-longitud [] = 0
-longitud (x:xs) = 1 + longitud xs
--- Ejercicio 8: decide si a dos usuarios les gustan las mismas publicaciones
-interseccion :: (Eq t) => [t] -> [t] -> [t]
-interseccion [] ys = []
-interseccion (x:xs) ys | pertenece ys x = [x] ++ interseccion xs ys
-                       | otherwise = interseccion xs ys
-
--- Ejercicio 9: decide si un usuario tiene un seguidor al que le gustan todas sus publicaciones
-likesEnComun :: [Publicacion] -> [Usuario]
-likesEnComun (p:[]) = likesDePublicacion p
-likesEnComun (p:ps) = interseccion (likesDePublicacion p) (likesEnComun ps)
-tieneUnSeguidorFiel :: RedSocial -> Usuario -> Bool
-tieneUnSeguidorFiel rs u = longitud (likesEnComun (publicacionesDe rs u)) > 0
-
--- Ejercicio 10: Indica si existe una cadena de relaciones con la cual desde el usuario 1 se llega al usuario 2
--- permutaciones
-insertar :: t -> Int -> [t] -> [t]
-insertar w 0 xs = [w] ++ xs
-insertar w at (x:xs) = [x] ++ insertar w (at-1) xs
-perm :: t -> [t] -> Int-> [[t]]
-perm y xs 0 = [[y] ++ xs]
-perm y xs l = [insertar y l xs] ++ perm y xs (l-1)
-permarray :: t -> [[t]] -> [[t]]
-permarray x [] = []
-permarray x (a:ac) = perm x a (longitud a) ++ permarray x ac
-_permutaciones :: [t] -> [[t]] -> [[t]]
-_permutaciones [] ac = ac
-_permutaciones (x:xs) ac = _permutaciones xs (permarray x ac)
-permutaciones :: [t] -> [[t]]
-permutaciones xs = _permutaciones xs [[]]
--- posicion de un elemento en una lista
-posicion :: (Eq t) => [t] -> t -> Int
-posicion (x:xs) y | x == y = 0
-                  | otherwise = 1 + posicion xs y
--- trozo de una lista desde indice fr hasta indice to
-slice :: [t] -> Int -> Int -> [t]
-slice [] fr to = []
-slice (x:xs) 0 0 = []
-slice (x:xs) 0 to = [x] ++ slice xs 0 (to-1)
-slice xs fr to = slice xs (fr-1) (to-1)
--- verifica que las relaciones todas las relaciones de la cadena de usuarios existan entre las Relaciones de la red social
-relacionesEnCadena :: [Relacion] -> [Usuario] -> Bool
-relacionesEnCadena rs (u:us) | longitud us>1 = (pertenece rs r1 || pertenece rs r2) && relacionesEnCadena rs us
-                             | otherwise = (pertenece rs r1 || pertenece rs r2)
-                             where r1 = (u, head us)
-                                   r2 = (head us, u)
--- toma cadenas de amigos validas a partir de las permutaciones de usuarios
-cadenaDeAmigos :: [Relacion] -> [[Usuario]] -> Usuario -> Usuario -> [Usuario]
-cadenaDeAmigos rs [] u1 u2 = []
-cadenaDeAmigos rs (a:as) u1 u2 | longitud s > 0 && relacionesEnCadena rs s = s
-                               | otherwise = cadenaDeAmigos rs as u1 u2
-                               where s = slice a (posicion a u1) (posicion a u2)
--- toma cadenas de amigos validas a partir de las permutaciones de usuarios
 existeSecuenciaDeAmigos :: RedSocial -> Usuario -> Usuario -> Bool
-existeSecuenciaDeAmigos rs u1 u2 = longitud (cadenaDeAmigos (relaciones rs) (permutaciones (usuarios rs)) u1 u2) > 0
+existeSecuenciaDeAmigos red u1 u2 = recorrerSecuencia red (amigosDe red u1) u2 [u1]
+
+recorrerSecuencia :: RedSocial -> [Usuario] -> Usuario -> [Usuario] -> Bool         -- La lista todosLosAmigos puede tener usuarios repetidos.
+recorrerSecuencia red [] b l = False                                                -- Se pueden eliminar con una funcion pero en este caso
+recorrerSecuencia red a u2 l | u2 `perteneceA` a = True                             -- no se hizo porque funciona y para no hacer mas largo
+                             | otherwise = siguientePosicion
+                             where siguientePosicion = recorrerSecuencia red (quitaUsuariosYaEvaluados (todosLosAmigos red a) l) u2 (l ++ a)
+
+todosLosAmigos :: RedSocial -> [Usuario] -> [Usuario]
+todosLosAmigos _ [] = []
+todosLosAmigos red (x:xs) = amigosDe red x ++ todosLosAmigos red xs
+
+quitaUsuariosYaEvaluados :: [Usuario] -> [Usuario] -> [Usuario]
+quitaUsuariosYaEvaluados [] l = []
+quitaUsuariosYaEvaluados (x:xs) l | x `perteneceA` l = quitaUsuariosYaEvaluados xs l
+                                  | otherwise = x : quitaUsuariosYaEvaluados xs l
+
